e72732f2f88b1013885ee3042d1a2f40ee7f3752
shyam-22 <shyambadgujar95@gmail.com>
2021-01-20 13:52:42 +0530

    #shyam : color codeing done here

--- frontend-ecommerce/src/CORE component/CardLayout.js
+++ frontend-ecommerce/src/CORE component/CardLayout.js
@@ -15,23 +15,39 @@ const CardLayout = ({product,showViewProductButton = true}) => {
                 )
             )
         }  
+
+        const showAddToCardButton = () => {
+            return (
+                <button type="submit" className="btn btn-outline-warning  mt-2 mb-2 ml-2">Add_To_Cart</button>
+            )
+        }
+
+        const showStock = (quantity) => {
+            return (
+                quantity > 0 
+                ? <span className="badge badge-primary badge-pill badge-center">In Stock</span>
+                : <span className="badge badge-warning badge-pill">Out of Stock</span>
+            )
+        }
     
     return (
         <div className="col-3 mb-2">
             <div className="card">
-                <div className="card-header text-center">{product.name}</div>
+                <div className="card-header text-center name">{product.name}</div>
                 <div className="card-body">
                     <center><ShowImage product={product} url={"product"}/>
                     <p className="lead mt-2">{product.description.substring(0,50)}</p>
-                    <p className="black-9">${product.price}</p>
-                    <p className="black-8">Category : {product.category && product.category.name}</p>
-                    <p className="black-9">Added On : {moment(product.createdAt).fromNow()}</p>
+                    <p className="black-10">${product.price}</p>
+                    <p className="black-9">Category : {product.category && product.category.name}</p>
+                    <p className="black-8">Added On : {moment(product.createdAt).fromNow()}</p>
 
 
                     </center>
+                    {showStock(product.quantity)}
+                    <br/>
                     {showViewButton(showViewProductButton)}
-                    <button type="submit" className="btn btn-outline-warning  mt-2 mb-2 ml-2">Add_To_Cart</button>
-
+                    {showAddToCardButton()}
+                   
                 </div>
             </div>
         </div>
