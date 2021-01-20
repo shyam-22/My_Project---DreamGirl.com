76cbabe88ea86bc33527cac54f6299f77b1d6238
shyam-22 <shyambadgujar95@gmail.com>
2021-01-20 13:25:45 +0530

    $shyam : proceed

--- frontend-ecommerce/src/CORE component/CardLayout.js
+++ frontend-ecommerce/src/CORE component/CardLayout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import ShowImage from "./showImage"
+import moment from "moment"
 
 const CardLayout = ({product,showViewProductButton = true}) => {
 
@@ -13,7 +14,7 @@ const CardLayout = ({product,showViewProductButton = true}) => {
                      </Link>
                 )
             )
-        } 
+        }  
     
     return (
         <div className="col-3 mb-2">
@@ -21,8 +22,12 @@ const CardLayout = ({product,showViewProductButton = true}) => {
                 <div className="card-header text-center">{product.name}</div>
                 <div className="card-body">
                     <center><ShowImage product={product} url={"product"}/>
-                    <p>{product.description.substring(0,50)}</p>
-                    <p>${product.price}</p>
+                    <p className="lead mt-2">{product.description.substring(0,50)}</p>
+                    <p className="black-9">${product.price}</p>
+                    <p className="black-8">Category : {product.category && product.category.name}</p>
+                    <p className="black-9">Added On : {moment(product.createdAt).fromNow()}</p>
+
+
                     </center>
                     {showViewButton(showViewProductButton)}
                     <button type="submit" className="btn btn-outline-warning  mt-2 mb-2 ml-2">Add_To_Cart</button>
